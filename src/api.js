cb2dc22cb9175fbb3724af08f1f6ed1f9f8b1e59
Court534 <courtney-stow@hotmail.co.uk>
2022-12-19 15:23:32 +0000

    Cleaned up the code in App.js and api.js

--- src/api.js
+++ src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const SearchImages = async () => {
+const SearchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
       Authorization: 'Client-ID RR7oFhACl8-SFbQs7rJd97OJ2vA5CcKEQiZacjKhIWs'
     },
     params: {
-      query: 'cats',
+      query: term,
     },
   });
 
-  console.log(response);
-  return response;
+  return response.data.results;
 };
 
 export default SearchImages;
