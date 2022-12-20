db2ea05e154d2f806897771c850f28bdd62504a7
Court534 <courtney-stow@hotmail.co.uk>
2022-12-20 11:34:29 +0000

    Getting user input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,22 @@
+import { useState } from 'react';
+
 function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState('');
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
     onSubmit('Cats')
   };
 
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
   <div>
     <form onSubmit={handleFormSubmit}>
-      <input />
+      <input onChange={handleChange}/>
     </form>
   </div>
   );
